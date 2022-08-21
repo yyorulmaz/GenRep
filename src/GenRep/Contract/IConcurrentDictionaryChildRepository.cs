231d47c5be423ff08d47f67a9d1c88ab84a72392
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2022-08-21 19:52:46 +0300

    concurrent dictionary child edit

--- src/GenRep/Contract/IConcurrentDictionaryChildRepository.cs
+++ src/GenRep/Contract/IConcurrentDictionaryChildRepository.cs
@@ -5,19 +5,17 @@ using System.Collections.Generic;
 namespace GenRep.Contract
 {
 
-    public interface IConcurrentDictionaryInstanceRepository<TKey, TValue>
+    public interface IConcurrentDictionaryChildRepository<TKey, TValue>
         where TKey : notnull
         where TValue : notnull
     {
         #region Data
         public IConcurrentDictionaryRepository<TKey, TValue> DataMain { get; }
         public ConcurrentDictionary<TKey, TValue> Data { get; }
-        public ConcurrentDictionary<TKey, TValue> DataTemp { get; }
         #endregion
 
         #region Count
         public int Count { get; }
-        public int CountTemp { get; }
         #endregion
 
         #region CRUD
@@ -25,18 +23,14 @@ namespace GenRep.Contract
         TValue Get(Func<TValue, bool> filter);
         List<TValue> GetAll(Func<TValue, bool> filter = null);
         bool Add(TKey key, TValue value);
-        bool AddTemp(TKey key, TValue value);
         bool Update(TKey key, TValue value);
         TValue Remove(TKey key);
-        bool RemoveAndAddTemp(TKey key);
         #endregion
 
         #region Changed
         public event Action<TValue> ChangedAdded;
         public event Action<TValue> ChangedUpdated;
         public event Action<TValue> ChangedRemoved;
-        public event Action<TValue> ChangedTempAdded;
-        public event Action<TValue> ChangedRemovedAndAddTemp;
         #endregion
     }
 }
